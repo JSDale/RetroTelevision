767cc5e83bd20e165d1390adafe62a368e0d97db
JSDALE <jdale994@googlemail.com>
2025-09-12 20:43:23 +0100

    starting from VScode with no AI agent

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RetroTelevision.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
