7f73a1cece7b5ee384b5f1acc8b077072c65e2f6
JSDALE <jdale994@googlemail.com>
2025-09-13 20:30:22 +0100

    adding the VLC control

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace RetroTelevision.ViewModels;
+﻿using LibVLCSharp.Shared;
+
+namespace RetroTelevision.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public MediaPlayer Player { get; }
+
+    private readonly LibVLC _vlcPlayer = new();
+
+    public MainWindowViewModel()
+    {
+        Core.Initialize();
+        Player = new MediaPlayer(_vlcPlayer);
+    }
+
+    public void OnLoad()
+    {
+        using var media = new Media(_vlcPlayer, @"", FromType.FromPath);
+        Player.Play(media);
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,22 @@
+using System;
 using Avalonia.Controls;
+using RetroTelevision.ViewModels;
 
 namespace RetroTelevision.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    private readonly MainWindowViewModel _viewModel;
+
+    public MainWindow(MainWindowViewModel viewModel)
     {
+        _viewModel = viewModel;
         InitializeComponent();
+        this.Opened += OpenComplete;
+    }
+
+    private void OpenComplete(object? obj, EventArgs args)
+    {
+        _viewModel.OnLoad();
     }
 }
